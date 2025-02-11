5d581a602d1ac6d6b3364f6c90015ccfa54d7f6c
drinbin <944958744@qq.com>
2025-02-12 00:24:38 +0800

    [feature]增加想法

--- app/client/platforms/openai.ts
+++ app/client/platforms/openai.ts
@@ -155,7 +155,9 @@ export class ChatGPTApi implements LLMApi {
             const text = msg.data;
             try {
               const json = JSON.parse(text);
-              const delta = json.choices[0].delta.content;
+              const delta =
+                json.choices[0].delta.reasoning_content ||
+                json.choices[0].delta.content;
               if (delta) {
                 responseText += delta;
                 options.onUpdate?.(responseText, delta);
