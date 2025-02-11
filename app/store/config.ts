5c5dcedea5bcd7717c389bffbff14e9db3d5dabe
drinbin <944958744@qq.com>
2025-02-12 00:33:53 +0800

    [improve]引入deepseek

--- app/store/config.ts
+++ app/store/config.ts
@@ -46,7 +46,7 @@ export const DEFAULT_CONFIG = {
   models: DEFAULT_MODELS as any as LLMModel[],
 
   modelConfig: {
-    model: "gpt-3.5-turbo" as ModelType,
+    model: "deepseek-r1" as ModelType,
     temperature: 0.5,
     top_p: 1,
     max_tokens: 2000,
@@ -55,7 +55,7 @@ export const DEFAULT_CONFIG = {
     sendMemory: true,
     historyMessageCount: 4,
     compressMessageLengthThreshold: 1000,
-    enableInjectSystemPrompts: true,
+    enableInjectSystemPrompts: false,
     template: DEFAULT_INPUT_TEMPLATE,
   },
 };
